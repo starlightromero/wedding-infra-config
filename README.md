43a8d548a4e0f9846e8374d804e7c0971a8075ba
Starlight Romero <starlight.romero@flatiron.com>
2022-02-27 10:30:56 -0800

    chore: wipe infra clean

--- README.md
+++ README.md
@@ -15,8 +15,6 @@
 | Name | Version |
 |------|---------|
 | <a name="provider_digitalocean"></a> [digitalocean](#provider\_digitalocean) | 2.17.0 |
-| <a name="provider_helm"></a> [helm](#provider\_helm) | 2.4.1 |
-| <a name="provider_kubernetes"></a> [kubernetes](#provider\_kubernetes) | 2.8.0 |
 
 ## Modules
 
@@ -26,15 +24,7 @@ No modules.
 
 | Name | Type |
 |------|------|
-| [helm_release.cert_manager](https://registry.terraform.io/providers/hashicorp/helm/2.4.1/docs/resources/release) | resource |
-| [helm_release.nginx_ingress_chart](https://registry.terraform.io/providers/hashicorp/helm/2.4.1/docs/resources/release) | resource |
-| [kubernetes_deployment.wedding](https://registry.terraform.io/providers/hashicorp/kubernetes/2.8.0/docs/resources/deployment) | resource |
-| [kubernetes_ingress.ingress](https://registry.terraform.io/providers/hashicorp/kubernetes/2.8.0/docs/resources/ingress) | resource |
-| [kubernetes_ingress.ingress_admin](https://registry.terraform.io/providers/hashicorp/kubernetes/2.8.0/docs/resources/ingress) | resource |
-| [kubernetes_namespace.wedding_app](https://registry.terraform.io/providers/hashicorp/kubernetes/2.8.0/docs/resources/namespace) | resource |
-| [kubernetes_service.wedding](https://registry.terraform.io/providers/hashicorp/kubernetes/2.8.0/docs/resources/service) | resource |
 | [digitalocean_kubernetes_cluster.this](https://registry.terraform.io/providers/digitalocean/digitalocean/2.17.0/docs/data-sources/kubernetes_cluster) | data source |
-| [digitalocean_loadbalancer.this](https://registry.terraform.io/providers/digitalocean/digitalocean/2.17.0/docs/data-sources/loadbalancer) | data source |
 
 ## Inputs
 
@@ -42,8 +32,6 @@ No modules.
 |------|-------------|------|---------|:--------:|
 | <a name="input_cluster_name"></a> [cluster\_name](#input\_cluster\_name) | The name of the kubernetes cluster to create | `string` | `"wedding-app"` | no |
 | <a name="input_do_token"></a> [do\_token](#input\_do\_token) | Access Token for Digital Ocean | `string` | n/a | yes |
-| <a name="input_hostname"></a> [hostname](#input\_hostname) | Hostname of website | `string` | `"charrington.xyz"` | no |
-| <a name="input_mongodb_uri"></a> [mongodb\_uri](#input\_mongodb\_uri) | Mongo database URI | `string` | n/a | yes |
 
 ## Outputs
 
--- main.tf
+++ main.tf
@@ -1,224 +1,224 @@
-data "digitalocean_loadbalancer" "this" {
-  name = "${var.cluster_name}-lb"
-}
+# data "digitalocean_loadbalancer" "this" {
+#   name = "${var.cluster_name}-lb"
+# }
 
-resource "kubernetes_namespace" "wedding_app" {
-  metadata {
-    name = var.cluster_name
-  }
-}
+# resource "kubernetes_namespace" "wedding_app" {
+#   metadata {
+#     name = var.cluster_name
+#   }
+# }
 
-resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "v1.7.1"
-  namespace  = "kube-system"
-  set {
-    name  = "createCustomResource"
-    value = "true"
-  }
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-}
+# resource "helm_release" "cert_manager" {
+#   name       = "cert-manager"
+#   repository = "https://charts.jetstack.io"
+#   chart      = "cert-manager"
+#   version    = "v1.7.1"
+#   namespace  = "kube-system"
+#   set {
+#     name  = "createCustomResource"
+#     value = "true"
+#   }
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
+# }
 
-# resource "kubernetes_manifest" "cluster_issuer" {
+# # resource "kubernetes_manifest" "cluster_issuer" {
+# #   depends_on = [
+# #     helm_release.cert_manager,
+# #   ]
+
+# #   manifest = {
+# #     "apiVersion" = "cert-manager.io/v1"
+# #     "kind"       = "ClusterIssuer"
+# #     "metadata" = {
+# #       "name" = "letsencrypt-production"
+# #     }
+# #     spec = {
+# #       acme = {
+# #         email  = "starlightromero@protonmail.com"
+# #         server = "https://acme-v02.api.letsencrypt.org/directory"
+# #         privateKeySecretRef = {
+# #           name = "letsencrypt-production"
+# #         }
+# #         solvers = [
+# #           {
+# #             http01 = {
+# #               ingress = {
+# #                 class = "nginx"
+# #               }
+# #             }
+# #           }
+# #         ]
+# #       }
+# #     }
+# #   }
+# # }
+
+# resource "helm_release" "nginx_ingress_chart" {
+#   name       = "nginx-ingress-controller"
+#   namespace  = var.cluster_name
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "nginx-ingress-controller"
+#   set {
+#     name  = "service.type"
+#     value = "LoadBalancer"
+#   }
+#   set {
+#     name  = "service.annotations.kubernetes\\.digitalocean\\.com/load-balancer-id"
+#     value = data.digitalocean_loadbalancer.this.id
+#   }
+# }
+
+# resource "kubernetes_ingress" "ingress" {
 #   depends_on = [
-#     helm_release.cert_manager,
+#     helm_release.nginx_ingress_chart,
 #   ]
-
-#   manifest = {
-#     "apiVersion" = "cert-manager.io/v1"
-#     "kind"       = "ClusterIssuer"
-#     "metadata" = {
-#       "name" = "letsencrypt-production"
+#   metadata {
+#     name      = "${var.cluster_name}-ingress"
+#     namespace = "kube-system"
+#     annotations = {
+#       "kubernetes.io/ingress.class"          = "nginx"
+#       "ingress.kubernetes.io/rewrite-target" = "/"
+#       "cert-manager.io/cluster-issuer"       = "letsencrypt-production"
 #     }
-#     spec = {
-#       acme = {
-#         email  = "starlightromero@protonmail.com"
-#         server = "https://acme-v02.api.letsencrypt.org/directory"
-#         privateKeySecretRef = {
-#           name = "letsencrypt-production"
-#         }
-#         solvers = [
-#           {
-#             http01 = {
-#               ingress = {
-#                 class = "nginx"
-#               }
-#             }
+#   }
+#   spec {
+#     rule {
+#       host = var.hostname
+#       http {
+#         path {
+#           backend {
+#             service_name = kubernetes_service.wedding.metadata[0].name
+#             service_port = 80
 #           }
-#         ]
+#           path = "/"
+#         }
 #       }
+#     }
+#     tls {
+#       secret_name = "letsencrypt-production"
+#       hosts       = [var.hostname]
 #     }
 #   }
 # }
 
-resource "helm_release" "nginx_ingress_chart" {
-  name       = "nginx-ingress-controller"
-  namespace  = var.cluster_name
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "service.annotations.kubernetes\\.digitalocean\\.com/load-balancer-id"
-    value = data.digitalocean_loadbalancer.this.id
-  }
-}
+# resource "kubernetes_ingress" "ingress_admin" {
+#   depends_on = [
+#     helm_release.nginx_ingress_chart,
+#   ]
+#   metadata {
+#     name      = "${var.cluster_name}-ingress-admin"
+#     namespace = "kube-system"
+#     annotations = {
+#       "kubernetes.io/ingress.class"                        = "nginx"
+#       "ingress.kubernetes.io/rewrite-target"               = "/"
+#       "cert-manager.io/cluster-issuer"                     = "letsencrypt-production"
+#       "nginx.ingress.kubernetes.io/whitelist-source-range" = "75.128.58.244/32"
+#     }
+#   }
+#   spec {
+#     rule {
+#       host = var.hostname
+#       http {
+#         path {
+#           backend {
+#             service_name = kubernetes_service.wedding.metadata[0].name
+#             service_port = 80
+#           }
+#           path = "/rsvp"
+#         }
+#       }
+#     }
+#     tls {
+#       secret_name = "letsencrypt-production"
+#       hosts       = [var.hostname]
+#     }
+#   }
+# }
 
-resource "kubernetes_ingress" "ingress" {
-  depends_on = [
-    helm_release.nginx_ingress_chart,
-  ]
-  metadata {
-    name      = "${var.cluster_name}-ingress"
-    namespace = "kube-system"
-    annotations = {
-      "kubernetes.io/ingress.class"          = "nginx"
-      "ingress.kubernetes.io/rewrite-target" = "/"
-      "cert-manager.io/cluster-issuer"       = "letsencrypt-production"
-    }
-  }
-  spec {
-    rule {
-      host = var.hostname
-      http {
-        path {
-          backend {
-            service_name = kubernetes_service.wedding.metadata[0].name
-            service_port = 80
-          }
-          path = "/"
-        }
-      }
-    }
-    tls {
-      secret_name = "letsencrypt-production"
-      hosts       = [var.hostname]
-    }
-  }
-}
+# resource "kubernetes_service" "wedding" {
+#   metadata {
+#     name      = "wedding"
+#     namespace = var.cluster_name
+#   }
 
-resource "kubernetes_ingress" "ingress_admin" {
-  depends_on = [
-    helm_release.nginx_ingress_chart,
-  ]
-  metadata {
-    name      = "${var.cluster_name}-ingress-admin"
-    namespace = "kube-system"
-    annotations = {
-      "kubernetes.io/ingress.class"                        = "nginx"
-      "ingress.kubernetes.io/rewrite-target"               = "/"
-      "cert-manager.io/cluster-issuer"                     = "letsencrypt-production"
-      "nginx.ingress.kubernetes.io/whitelist-source-range" = "75.128.58.244/32"
-    }
-  }
-  spec {
-    rule {
-      host = var.hostname
-      http {
-        path {
-          backend {
-            service_name = kubernetes_service.wedding.metadata[0].name
-            service_port = 80
-          }
-          path = "/rsvp"
-        }
-      }
-    }
-    tls {
-      secret_name = "letsencrypt-production"
-      hosts       = [var.hostname]
-    }
-  }
-}
+#   spec {
+#     selector = {
+#       app = kubernetes_deployment.wedding.metadata[0].labels.app
+#     }
+#     port {
+#       port        = 80
+#       target_port = 8080
+#     }
 
-resource "kubernetes_service" "wedding" {
-  metadata {
-    name      = "wedding"
-    namespace = var.cluster_name
-  }
+#     type = "ClusterIP"
+#   }
+# }
 
-  spec {
-    selector = {
-      app = kubernetes_deployment.wedding.metadata[0].labels.app
-    }
-    port {
-      port        = 80
-      target_port = 8080
-    }
+# resource "kubernetes_deployment" "wedding" {
+#   metadata {
+#     name      = "wedding"
+#     namespace = var.cluster_name
+#     labels = {
+#       app = "wedding"
+#     }
+#   }
 
-    type = "ClusterIP"
-  }
-}
+#   spec {
+#     replicas = 3
 
-resource "kubernetes_deployment" "wedding" {
-  metadata {
-    name      = "wedding"
-    namespace = var.cluster_name
-    labels = {
-      app = "wedding"
-    }
-  }
+#     selector {
+#       match_labels = {
+#         app = "wedding"
+#       }
+#     }
 
-  spec {
-    replicas = 3
+#     template {
+#       metadata {
+#         labels = {
+#           app = "wedding"
+#         }
+#       }
 
-    selector {
-      match_labels = {
-        app = "wedding"
-      }
-    }
+#       spec {
+#         container {
+#           image             = "starlightromero/wedding-app"
+#           name              = "wedding-app"
+#           image_pull_policy = "Always"
 
-    template {
-      metadata {
-        labels = {
-          app = "wedding"
-        }
-      }
+#           port {
+#             container_port = 8080
+#           }
 
-      spec {
-        container {
-          image             = "starlightromero/wedding-app"
-          name              = "wedding-app"
-          image_pull_policy = "Always"
+#           env {
+#             name  = "MONGODB_URI"
+#             value = var.mongodb_uri
+#           }
 
-          port {
-            container_port = 8080
-          }
+#           resources {
+#             limits = {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests = {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
 
-          env {
-            name  = "MONGODB_URI"
-            value = var.mongodb_uri
-          }
+#           liveness_probe {
+#             http_get {
+#               path = "/health"
+#               port = 8080
+#             }
 
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/health"
-              port = 8080
-            }
-
-            initial_delay_seconds = 5
-            period_seconds        = 5
-          }
-        }
-      }
-    }
-  }
-}
+#             initial_delay_seconds = 5
+#             period_seconds        = 5
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
